d999e05d8238107d26e403d4208b5bb31c676937
chibyke <chibykepride@gmail.com>
2023-12-20 12:47:59 +0000

    Deployed SmilingShark.sol successfully using DeploySmilingShark.s.sol

--- script/DeploySmilingShark.s.sol
+++ script/DeploySmilingShark.s.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.2;
 import {Script} from "forge-std/Script.sol";
 import {SmilingShark} from "../src/SmilingShark.sol";
 
-contract DeploySmilingShark is Script {}
+contract DeploySmilingShark is Script {
+    function run() external returns(SmilingShark) {
+        vm.startBroadcast();
+        SmilingShark smilingShark = new SmilingShark();
+        vm.stopBroadcast();
+        return smilingShark;
+    }
+}
