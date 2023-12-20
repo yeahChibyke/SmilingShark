0eac6d59a7734790ef2d902d209206a1c9da2ecb
chibyke <chibykepride@gmail.com>
2023-12-20 12:28:47 +0000

    started working on DeploySmilingShark.s.sol

---
+++ script/DeploySmilingShark.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import {Script} from "forge-std/Script.sol";
+import {SmilingShark} from "../src/SmilingShark.sol";
+
+contract DeploySmilingShark is Script {}
